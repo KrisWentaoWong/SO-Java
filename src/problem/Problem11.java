8d60639c9d9539b051cd6a89b4b19851e9693e07
KrisWentaoWong <wwt0812@outlook.com>
2019-05-13 13:28:51 +0800

    第11题

--- src/problem/Problem11.java
+++ src/problem/Problem11.java
@@ -11,6 +11,7 @@ public class Problem11 {
 
     public static int NumberOf1(int n) {
         int count = 0;
+        //int类型一共32位，依次判断每一位
         for(int i=0;i<32;++i){
             if((n >> i & 1) == 1){
                 count++;
