f3a96d85eb08dd96cd8fdf6c413a7c3f2ace05f9
KrisWentaoWong <wwt0812@outlook.com>
2019-05-21 17:56:23 +0800

    leetcode23-牛逼的解法

--- src/leetcode/MergeKsortedLists.java
+++ src/leetcode/MergeKsortedLists.java
@@ -28,6 +28,12 @@ public class MergeKsortedLists {
         mergeKLists(lists);
 
     }
+
+    /**
+     * 用最小堆每次取出最小的元素，复杂度为nklog(k)
+     * @param lists
+     * @return
+     */
     public static ListNode mergeKLists(ListNode[] lists) {
         int length = lists.length;
         if(length == 0){
@@ -93,6 +99,44 @@ public class MergeKsortedLists {
         }
         return newHead;
 
+    }
+
+    /**
+     * 用二分法归并排序，牛逼！！！
+     * @param lists
+     * @return
+     */
+    public static ListNode mergeKLists2(ListNode[] lists){
+        return partition(lists,0,lists.length-1);
+    }
+
+    public static ListNode partition(ListNode[] lists,int s,int e){
+        if(s == e){
+            return lists[s];
+        }else if(s < e){
+            int m = (s+e)/2;
+            ListNode l1 = partition(lists,s,m);
+            ListNode l2 = partition(lists,m+1,e);
+            return merge(l1,l2);
+        }else{
+            return null;
+        }
+    }
+
+    public static ListNode merge(ListNode l1,ListNode l2){
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val){
+            l1.next = merge(l1.next,l2);
+            return l1;
+        }else{
+            l2.next = merge(l1,l2.next);
+            return l2;
+        }
     }
     static class ListNode {
       int val;
